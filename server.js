a7d77da42b4ff01a88465dbb3d2230c0c98eda4e
kirillonishuk <kirillonishuk@yandex.by>
2018-08-21 22:01:35 +0300

    v0.2.5

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use(function (req, res, next) {
